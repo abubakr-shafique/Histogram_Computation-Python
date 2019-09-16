5436e8d281b56224402cc1f013b1929898686455
abubakr-shafique <54443371+abubakr-shafique@users.noreply.github.com>
2019-09-17 00:00:25 +0800

    Calculate Histogram

--- SimpleHistogram_GrayScaleImage.py
+++ SimpleHistogram_GrayScaleImage.py
@@ -1,39 +1,40 @@
-##This Program is written by Abubakr Shafique (abubakr.shafique@gmail.com)
-import numpy as np
-import cv2 as cv
+# This program is written by Abubakr Shafique (abubakr.shafique@gmail.com) 
+import numpy as np	#This is to deal with numbers and arrays
+import cv2 as cv	#This is to deal with images
 from matplotlib import pyplot as plt
 
-#This Histogram Function Takes in a Mat Image and Calculate Histogram
-def Calculate_Histogram(Image):
-	Image_Height = Image.shape[1] #get the Height of the Image
-	Image_Width = Image.shape[0] #get the Width of the Image
-
-	Hist = np.zeros([256], np.int32) #Initialize an empty 256 array
+def Histogram_Computation(Image):
+	Image_Height = Image.shape[0]
+	Image_Width = Image.shape[1]
 	
-	for x in range(0,Image_Width): # loop through the Image
-		for y in range(0,Image_Height):
-			Hist[Image[x,y]] +=1 # increment the same intensity values
+	Histogram = np.zeros([256], np.int32)
 	
-	return Hist #return the histogram
+	for x in range(0, Image_Height):
+		for y in range(0, Image_Width):
+			Histogram[Image[x,y]] +=1
+	
+	return Histogram
 
-def Plot_Histogram(Histogram_GrayScale):
+def Plot_Histogram(Histogram):
 	plt.figure()
-	plt.title("Grayscale Histogram")
+	plt.title("GrayScale Histogram")
 	plt.xlabel("Intensity Level")
 	plt.ylabel("Intensity Frequency")
-	plt.xlim([0, 256]) # Defined the limit of x-axis
-	plt.plot(Histogram_GrayScale)
-	plt.savefig("GrayScale_Histogram.jpg")
+	plt.xlim([0, 256])
+	plt.plot(Histogram)
+	plt.savefig("Histogram_GrayScale.jpg")
 
 def main():
-	Input_Image = cv.imread('Test_Image.png',0) #This will read a GrayScale Image (Even if it is RGB it will read GrayScale)
-	Histogram_GrayScale = Calculate_Histogram(Input_Image)
-
+	Input_Image = cv.imread("Test_Image.png",0) # This is to read Gray Scale Image
+	
+	Histogram_GrayScale = Histogram_Computation(Input_Image)
+	
+	#Now to print our output Histogram
 	for i in range(0,len(Histogram_GrayScale)):
-		print('Histogram[',i,']: ', Histogram_GrayScale[i])
-		
+		print("Histogram[",i,"]: ", Histogram_GrayScale[i])
 	Plot_Histogram(Histogram_GrayScale)
-	input("Press Enter to continue...")
+	input("Please Enter to Continue...")
+	
 
 if __name__ == '__main__':
-	main() #Our main function
+	main()
